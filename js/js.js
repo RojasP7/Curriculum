6b7c1c77c53669b98766d64a4fb82ebf82eed2b2
RojasP7 <ferojasp@ufpso.edu.co>
2024-04-26 14:33:36 -0500

    upload

--- js/js.js
+++ js/js.js
@@ -3,10 +3,10 @@ let ico_hamburguer = true;
 
 document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
     if(ico_hamburguer){
-        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
+        document.querySelectorAll(".hamburguer>i")[0].style.color ="#fff";
         ico_hamburguer= false;
     }else{
-        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        document.querySelectorAll(".hamburguer>i")[0].style.color ="#000";
         ico_hamburguer= true;
     }
 
